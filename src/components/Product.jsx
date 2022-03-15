efb63a2500e42b6fec12809c7849f949704e0615
FA1312 <faldrovandi83@gmail.com>
2022-03-15 12:19:04 +0100

    fixed dependency on editproduct

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -22,6 +22,7 @@ function Product() {
       .catch(error => {
         console.log(error);
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleDelete = async () => {
